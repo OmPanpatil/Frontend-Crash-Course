6f96752b91a52217f723b1e99927a77d71e360fe
Om Panpatil <ompanpatil513@gmail.com>
2025-01-18 21:30:59 +0530

    JS

---
+++ JS_Learning/script.js
@@ -0,0 +1,45 @@
+console.log("Hello World");
+alert("Facing some connection issues, please try again later");
+prompt("Please tell us your suitable time?");
+
+// Functions --> 
+// block of code that can be called again and again
+// Reusability of code
+// Js mein function normal programming language jesse nahi hai, iska matlab hai, yaha par js mein, functions banane ke liye type nahi banana padta.
+// Js mein function banane ke liye, function keyword ka use hota hai. Kyuki yaha par function keyword se hi pata chalta hai ki yeh ek function hai. Aur data type ka issue nahi hota hai.
+// Js mein function banane ke liye, function keyword ke baad function ka naam hota hai, jisse hum function ko call kar sakte hain.
+// That's why, js mein sirf function keyword likh sakhte hai bina data type ke.
+
+
+// Js versions have 2 types:
+// 1. ES5
+// 2. ES6
+
+// 1. ES5 : Function statement, function expression, anonymous function
+// 2. ES6 : Fat Arrow functions --> a) Basic Fat Arrow, b) Fat arrow with one parameter, c) Fat arrow with implicit return
+
+// JS functions are divided into 2 parts:
+// 1. Built-in functions
+// 2. User-defined functions
+// Built-in functions are already defined in JS
+// Eg: alert(), prompt(), console.log(), etc.
+// User-defined functions are defined by the user
+// Eg: function myFunction() { console.log("Hello World"); }
+
+// JS functions have 3 parts:
+// 1. function keyword
+// example: function
+// 2. Function name
+// example: myFunction
+// 3. Parentheses () with parameters
+// example: myFunction()
+
+// Syntax of function:
+function functionname(){
+    // code to be executed
+    console.log("Syntax of Function");
+}
+
+functionname(); // calling function
+
+// Indepth some more important points: js mein functions first class functions hote hain, yani ki js mein functions ek variable ki tarah use ho sakte hain.
